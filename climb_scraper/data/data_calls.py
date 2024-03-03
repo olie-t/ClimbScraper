43f360f034a3e414156dc2c2f331ccebceb404c8
Olie <olthomas13@gmail.com>
2024-03-03 22:27:37 +0000

    adding framwork for data functions

---
+++ climb_scraper/data/data_calls.py
@@ -0,0 +1,22 @@
+def get_crag_id():
+    pass
+
+
+def put_crag_id():
+    pass
+
+
+def get_crag_stats():
+    pass
+
+
+def put_crag_stats():
+    pass
+
+
+def climb_data_table():
+    pass
+
+
+def crag_id_table():
+    pass
