8808bccf1d30e878dac6b5d441ff0e4a90fb0616
olie-t <olthomas13@gmail.com>
2024-05-31 12:07:15 +0100

    starting protyping

--- climb_scraper/scraper/scraper.py
+++ climb_scraper/scraper/scraper.py
@@ -1,90 +1,52 @@
-import pandas as pd
-from bs4 import BeautifulSoup as bs
 import requests
 import json
-from sqlalchemy import create_engine
-from Connection_test import connection_string
-def climb_scrape():
+import bs4 as bs
+
+
+def get_data(url: str) -> json:
+    """Attempts to get data from the provided URL"""
+
     try:
-        with open('crag_ids.txt', 'r') as crag_file:
-            crag_id = crag_file.readline()
+        response = requests.get(url)
+        return response
+    except Exception as e:
+        print(f"Get Data request failed with error:\n{e}")
 
-        engine = create_engine(connection_string)
+def data_to_string(data: json) -> str:
+    """Processes the raw response into a string for processing"""
 
-        site = f"https://www.ukclimbing.com/logbook/crag.php?id={crag_id}"
-        results = requests.get(site)
-        soup = bs(results.content, 'html.parser')
+    try:
+        soup = bs(data.content, 'html.parser')
         scripts = soup.find_all('script')
-        climb_table = scripts[10].string
+        climb_table_string = scripts[10].string
+        return climb_table_string
+    except Exception as e:
+        print(f"Data processor request failed with error:\n{e}")
 
-        start_keyword = 'table_data = ['
-        start_index = climb_table.find(start_keyword) + len('table_data = ')
-        open_brackets = 0
-        close_brackets = 0
-        i = start_index
 
+def string_processor_climbs(data: str) -> json:
+    """Find the relevant data for both the climbs and the grading at this crag"""
+
+    start_keyword = 'table_data = ['
+    try:
+        start_index = data.find(start_keyword) + len(start_keyword)
+    except Exception as e:
+        print(f'Unable to find start keyword in data string:\n{e}')
+    open_brackets = 0
+    closed_brackets = 0
+    i = start_index
+
+    try:
         while True:
-            if climb_table[i] == '[':
+            if data[i] == '[':
                 open_brackets += 1
-            elif climb_table[i] == ']':
-                close_brackets += 1
-            if open_brackets == close_brackets and open_brackets != 0:
+            elif data[i] == ']':
+                closed_brackets += 1
+            if open_brackets == closed_brackets and open_brackets != 0:
                 end_index = i + 1
                 break
             i += 1
-        print("accessing json")
-        json_data = climb_table[start_index:end_index]
-        climbs = json.loads(json_data)
-        climbs_df = pd.DataFrame(climbs)
-        filtered_climbs_df = climbs_df.filter(['id', 'name', 'grade', 'techgrade', 'gradesystem', 'gradetype', 'gradescore'], axis=1)
-
-        grades_start_keyword = 'grade_list = '
-        grade_start_index = climb_table.find(grades_start_keyword) + len('grade_list = ')
-        open_swirlies = 0
-        close_swirlies = 0
-        j = grade_start_index
-        while True:
-            if climb_table[j] == '{':
-                open_swirlies += 1
-            elif climb_table[j] == '}':
-                close_swirlies += 1
-            if open_swirlies == close_swirlies and open_swirlies != 0:
-                grade_end_index = j + 1
-                break
-            j += 1
-        json_grades = climb_table[grade_start_index:grade_end_index]
-        grades = json.loads(json_grades)
-        grades_list = []
-        for outer_key, outer_value in grades.items():
-            for inner_key, inner_value in outer_value.items():
-                row = inner_value.copy()
-                if 'alt' in row:
-                    alt_data = row.pop('alt')
-                    for k, v in alt_data.items():
-                        row[f'alt_{k}'] = v
-                grades_list.append(row)
-        print("pushing to df")
-        grades_df = pd.DataFrame(grades_list)
-
-        climbs_w_grades = pd.merge(
-            filtered_climbs_df, grades_df, left_on='grade', right_on='id')
-        climbs_w_grades.drop(['grade', 'gradesystem_x', 'id_y', 'gradesystem_y', 'score', 'gradecolor', 'alt_id', 'alt_name'], axis=1, inplace=True, errors="ignore")
-        climbs_w_grades.rename(columns={'id_x': 'climb_id', 'name_x': 'climb_name', 'name_y': 'climb_grade'}, inplace=True)
-        climbs_w_grades = climbs_w_grades[['climb_id', 'climb_name', 'climb_grade', 'techgrade', 'gradescore', 'gradetype']]
-
-        climbs_w_grades.to_sql(name="climbing_data", con=engine, if_exists="append", index=False)
-        print(f"crag {crag_id} pushed to DB successfuly")
-
+        json_data = data[start_index:end_index]
+        return json_data
     except Exception as e:
-        with open('error_log.txt', 'a') as log_file:
-            log_file.write(f"Encounted error - {e} while processing crag  id {crag_id}")
-        print(e)
-
-    finally:
-        with open("crag_ids.txt", "r+") as crag_file:
-            crags = crag_file.readlines()
-            crag_file.seek(0)
-            crag_file.truncate()
-            crag_file.writelines(crags[1:])
-
-climb_scrape()
+        print(f'String processing failed to correctly determine the bounds of the json with error:\n{e}')
